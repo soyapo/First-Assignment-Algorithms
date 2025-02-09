db731d251e2b9e9e71e0e4cbdefad2ea93a89c3b
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-02-09 22:56:07 +0330

    feat(exercise): designed problems

---
+++ AP1403 - Algorithms/src/main/java/Exercises.java
@@ -0,0 +1,59 @@
+public class Exercises {
+    /*
+        there is an array of integers as input of function and another integer for the target value
+        all the algorithm should do is to find those two integers in array which their multiplication is the target
+        then it should return an array of their indices
+        e.g. {1, 2, 3, 4} with target of 8 -> {1, 3}
+    */
+    public int[] productIndices(int[] values, int target) {
+        // todo
+        return null;
+    }
+
+    /*
+        given a matrix of random integers, you should do spiral traversal in it
+        e.g. if the matrix is as shown below:
+        1 2 3
+        4 5 6
+        7 8 9
+        then the spiral traversal of that is:
+        {1, 2, 3, 6, 9, 8, 7, 4, 5}
+
+        so you should walk in that matrix in a curl and then add the numbers in order you've seen them in a 1D array
+    */
+    public int[] spiralTraversal(int[][] values, int rows, int cols) {
+        // todo
+        return null;
+    }
+
+    /*
+        integer partitioning is a combinatorics problem in discreet maths
+        the problem is to generate sum numbers which their summation is the input number
+
+        e.g. 1 -> all partitions of integer 3 are:
+        3
+        2, 1
+        1, 1, 1
+
+        e.g. 2 -> for number 4 goes as:
+        4
+        3, 1
+        2, 2
+        2, 1, 1
+        1, 1, 1, 1
+
+        note: as you can see in examples, we want to generate distinct summations, which means 1, 2 and 2, 1 are no different
+        you should generate all partitions of the input number and
+
+        hint: you can measure the size of array by finding the pattern of partitions and their number
+        trust me, that one's fun and easy :)
+    */
+    public int[][] intPartitions(int n) {
+        // todo
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // you can test your code here
+    }
+}
